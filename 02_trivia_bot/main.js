474dcd7ed3dec1be20b802354c3b82cee1f1b5f4
Madlion0187 <norbi0187@gmail.com>
2026-06-27 22:06:29 +0200

    trivia bot

---
+++ 02_trivia_bot/main.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Mikrobi";
+let botLocation = "Europe";
+let favoriteLanguage = "Python";
+
+console.log("My name is " + botName + " and I live on " + botLocation  + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "I like to coding in " + favoriteLanguage + ".";
+console.log(codingFact);
+
+favoriteLanguage = "JavaScript";
+codingFact = "I also like to coding in " + favoriteLanguage + ".";
+console.log(codingFact);
+
+favoriteLanguage = "Java";
+codingFact = "I also like to coding in " + favoriteLanguage + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
